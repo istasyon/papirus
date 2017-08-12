cbf1842189ba0c4c7aee2e7834ba3ece768cc2f0
Egemen Can Uze <egemencanuze@gmail.com>
2017-08-12 15:01:16 +0200

    Add search input box

--- client/src/components/Landing/Hero.js
+++ client/src/components/Landing/Hero.js
@@ -1,9 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import LandingNavbar from './LandingNavbar';
-import './styles/Hero.scss';
+import LandingNavbar from "./LandingNavbar";
+import CATEGORIES from "../../constants/categories";
+import "./styles/Hero.scss";
 
 class Hero extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchValue: "",
+      categories: CATEGORIES
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.searchValue);
+    this.setState({ searchValue: "" });
+  }
+
   render() {
     return (
       <section className="Hero">
@@ -13,7 +29,31 @@ class Hero extends Component {
             <h1 className="Hero__title">
               Find local and online tutors at affordable prices
             </h1>
-            <button className="Hero__button">SEARCH TUTORS</button>
+            <form onSubmit={this.handleSubmit}>
+              <div>
+                <input
+                  type="text"
+                  list="categories"
+                  className="Hero__searchinput"
+                  placeholder="What do you want to learn?"
+                  value={this.state.searchValue}
+                  onChange={event =>
+                    this.setState({ searchValue: event.target.value })}
+                />
+                <datalist id="categories">
+                  {this.state.categories.map(item =>
+                    <option key={item} value={item} />
+                  )}
+                </datalist>
+              </div>
+              <button
+                type="submit"
+                className="Hero__button"
+                onClick={this.handleSubmit}
+              >
+                SEARCH TUTORS
+              </button>
+            </form>
           </div>
         </div>
       </section>
---
+++ client/src/constants/categories.js
@@ -0,0 +1,14 @@
+const CATEGORIES = [
+  "Tennis",
+  "Basketball",
+  "Football",
+  "Math",
+  "Music",
+  "Chess",
+  "Russian",
+  "English",
+  "German",
+  "Yoga"
+];
+
+export default CATEGORIES;
